ccede131b7c372ab8c552b9cb91c63dec5e760d3
“tatsuki0091” <rossosalvatore@icloud.com>
2021-06-21 16:29:14 -0700

    Complete register function.

--- api/controllers/comment.py
+++ api/controllers/comment.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response, request, Blueprint, jsonify
+from api.models import Comment, CommentSchema
 
 # To call method in this class
 comment = Blueprint('comment', __name__, url_prefix='/')
@@ -6,10 +7,13 @@ comment = Blueprint('comment', __name__, url_prefix='/')
 @comment.route('/api/comment', methods=['POST', 'GET'])
 def postComment():
     if request.method == 'POST':
+        comment_result = Comment.registerComment(request.json)
+        print(comment_result)
         res = {
-            'message': 'post'
+            'code': 200,
+            'user_id': comment_result.user_id,
+            'text': comment_result.text,
         }
-
         return jsonify(res), 200
     else:
         res = {
--- api/models/comment.py
+++ api/models/comment.py
@@ -9,7 +9,7 @@ class Comment(db.Model):
 
     # Define column of table
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     text = db.Column(db.String(250),  nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
@@ -19,7 +19,23 @@ class Comment(db.Model):
         self.user_id = user_id
         self.text = text
 
+    def registerComment(comment):
+        record = Comment(
+            user_id=comment['user_id'],
+            text=comment['text'],
+        )
+        try:
+            db.session.add(record)
+            db.session.commit()
+            return record
+        except Exception as e:
+            return e
+
+
+
 class CommentSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         # Apporopriate model properties for all schima
         model = Comment
+
+
